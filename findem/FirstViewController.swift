584a93d5c33f7106ccb0330c022bbf371b590187
jsaya01 <jsaya01@gmail.com>
2019-04-29 11:01:41 -0700

    initial commit

---
+++ findem/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  findem
+//
+//  Created by Jonah Saya on 4/29/19.
+//  Copyright © 2019 Jonah Saya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ findem/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  findem
+//
+//  Created by Jonah Saya on 4/29/19.
+//  Copyright © 2019 Jonah Saya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
